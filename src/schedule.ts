db8b37a39e93ed07cb7a54b075bd0c6154d6bc37
李志鹏 <lizhipeng.lzp@bytedance.com>
2021-10-26 12:26:21 +0800

    fix: 定时器修改

--- src/schedule.ts
+++ src/schedule.ts
@@ -1,12 +1,22 @@
-import { scheduleJob } from 'node-schedule';
-import { workspace, window, Uri, Disposable } from 'vscode';
-import { getConfig } from './getConfig';
-import { getText } from './noInvolutionText';
+import { scheduleJob } from "node-schedule";
+import { workspace, window, Uri, Disposable } from "vscode";
+import { getConfig } from "./getConfig";
+import { getText } from "./noInvolutionText";
 
 export const subscribeSchedule = (config: ReturnType<typeof getConfig>) => {
   if (config.enable) {
-    const { enableGitHook, startWorkHour, startWorkMin, endWorkHour, endWorkMin, endingHours, startSeconds, endSeconds } = config;
-    const nowSeconds = new Date().getHours() * 3600 + new Date().getMinutes() * 60;
+    const {
+      enableGitHook,
+      startWorkHour,
+      startWorkMin,
+      endWorkHour,
+      endWorkMin,
+      endingHours,
+      startSeconds,
+      endSeconds,
+    } = config;
+    const nowSeconds =
+      new Date().getHours() * 3600 + new Date().getMinutes() * 60;
     let saveDisposable: Disposable | null = null;
     let statusBarTimer: NodeJS.Timeout | null = null;
     if (enableGitHook) {
@@ -16,71 +26,99 @@ export const subscribeSchedule = (config: ReturnType<typeof getConfig>) => {
     }
     if (nowSeconds < startSeconds || nowSeconds >= endSeconds) {
       endCb();
-    } else if (nowSeconds >= startSeconds && nowSeconds < endSeconds - endingHours * 3600) {
+    } else if (
+      nowSeconds >= startSeconds &&
+      nowSeconds < endSeconds - endingHours * 3600
+    ) {
       startCb();
-    } else if (nowSeconds >= endSeconds - endingHours * 3600 && nowSeconds < endSeconds) {
+    } else if (
+      nowSeconds >= endSeconds - endingHours * 3600 &&
+      nowSeconds < endSeconds
+    ) {
       endingCb();
     }
 
     scheduleJob(`0 ${startWorkMin} ${startWorkHour} * * *`, () => {
       startCb();
     });
-    scheduleJob(`0 ${Math.floor((startSeconds + endingHours * 3600) / 3600)} ${((startSeconds + endingHours * 3600) % 3600) / 60} * * *`, () => {
-      endingCb();
-    });
+    scheduleJob(
+      `0 ${Math.floor((startSeconds + endingHours * 3600) / 3600)} ${
+        ((startSeconds + endingHours * 3600) % 3600) / 60
+      } * * *`,
+      () => {
+        endingCb();
+      }
+    );
     scheduleJob(`0 ${endWorkHour} ${endWorkMin} * * *`, () => {
       endCb();
     });
     function startCb() {
       statusBarTimer && clearInterval(statusBarTimer);
-      window.setStatusBarMessage('');
+      window.setStatusBarMessage("");
       saveDisposable && saveDisposable.dispose();
     }
 
     function endingCb() {
       saveDisposable && saveDisposable.dispose();
       setStatusBar();
-      window.showInformationMessage('已进入今日工作收尾阶段，请尽快收尾手中工作，准备下班。');
+      window.showInformationMessage(
+        "已进入今日工作收尾阶段，请尽快收尾手中工作，准备下班。"
+      );
     }
 
     function endCb() {
       setStatusBar();
-      saveDisposable = workspace.onDidSaveTextDocument(document => {
+      saveDisposable = workspace.onDidSaveTextDocument((document) => {
         console.log(workspace.name);
         showInputBox(document.uri);
-        window.showInformationMessage('现在是下班时间！');
+        window.showInformationMessage("现在是下班时间！");
       });
     }
 
     function setStatusBar() {
-      !statusBarTimer && (statusBarTimer = setInterval(() => {
-        window.setStatusBarMessage(getText());
-      }, 10000));
+      !statusBarTimer &&
+        (statusBarTimer = mySetInterval(() => {
+          window.setStatusBarMessage(getText());
+        }, 10000));
     }
   }
 };
 
-
+function mySetInterval(fn: (arg?: any) => any, millisec: number) {
+  function interval() {
+    setTimeout(interval, millisec);
+    fn();
+  }
+  return setTimeout(interval, millisec);
+}
 
 function showInputBox(uri: Uri) {
-  window.showInputBox({
-    placeHolder: '请输入工作区名称',
-    prompt: '非工作时间必须要输入正确的工作区名称才能保存，否则会清空当前文件~'
-  }).then(str => {
-    if (str) {
-      if (str !== workspace.name) {
-        showInputBox(uri);
+  window
+    .showInputBox({
+      placeHolder: "请输入工作区名称",
+      prompt:
+        "非工作时间必须要输入正确的工作区名称才能保存，否则会清空当前文件~",
+    })
+    .then((str) => {
+      if (str) {
+        if (str !== workspace.name) {
+          showInputBox(uri);
+        }
+      } else {
+        workspace.fs.writeFile(uri, Buffer.from(""));
       }
-    } else {
-      workspace.fs.writeFile(uri, Buffer.from(''));
-    }
-  });
+    });
 }
 
 function addPreCommit(startSeconds: number, endSeconds: number) {
   if (workspace.workspaceFolders?.length) {
-    const hooksUri = Uri.joinPath(workspace.workspaceFolders![0].uri, '/.git/hooks');
-    workspace.fs.writeFile(Uri.joinPath(hooksUri, 'pre-commit'), Buffer.from(`#!/usr/bin/env node
+    const hooksUri = Uri.joinPath(
+      workspace.workspaceFolders![0].uri,
+      "/.git/hooks"
+    );
+    workspace.fs.writeFile(
+      Uri.joinPath(hooksUri, "pre-commit"),
+      Buffer.from(`#!/usr/bin/env node
 const nowSeconds = new Date().getHours() * 3600 + new Date().getMinutes() * 60;
 const startSeconds = ${startSeconds}
 const endSeconds = ${endSeconds}
@@ -90,14 +128,18 @@ if (nowSeconds < startSeconds || nowSeconds > endSeconds) {
 } else {
   process.exit(0)
 }
-        `));
+        `)
+    );
   }
 }
 function removePreCommit() {
   if (workspace.workspaceFolders?.length) {
-    const hooksUri = Uri.joinPath(workspace.workspaceFolders![0].uri, '/.git/hooks');
+    const hooksUri = Uri.joinPath(
+      workspace.workspaceFolders![0].uri,
+      "/.git/hooks"
+    );
     try {
-      workspace.fs.delete(Uri.joinPath(hooksUri, 'pre-commit'));
+      workspace.fs.delete(Uri.joinPath(hooksUri, "pre-commit"));
     } catch (error) {
       console.log(error);
     }
